3c8d2e1fd6d5225c1cb757088e75547b0441e053
CodeCraftKJ <krzysztofjofficial@gmail.com>
2023-12-24 00:06:11 +0100

    Update CM.jsx

--- src/app/components/CM.jsx
+++ src/app/components/CM.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import MapCard from "./MapCard";
-import maps from "../../../public/data/maps";
+import maps from "../../../public/data/maps"; // Ensure this is the correct path to your maps array
 
 const CM = () => {
   const [selectedMap, setSelectedMap] = useState(null);
@@ -9,47 +9,45 @@ const CM = () => {
 
   useEffect(() => {
     const pseudoRandom = (seed, max) => {
-      let value = (seed * 9301 + 49297) % 233280;
+      let value = (seed * 9301 + 49297) % 233280; // Simple PRNG
       return Math.floor((value / 233280) * max);
     };
 
-    const getMapIndex = () => {
-      const londonTime = new Date(
-        new Date().toLocaleString("en-US", { timeZone: "Europe/London" })
+    const getCurrentSeed = () => {
+      const now = new Date();
+      const startOfDay = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate()
       );
-      const seed =
-        Math.floor(londonTime.getTime() / (60000 * 15)) % maps.length;
-      return pseudoRandom(seed, maps.length);
+      const minutesSinceStartOfDay = Math.floor((now - startOfDay) / 60000);
+      return Math.floor(minutesSinceStartOfDay / 15); // Change seed every 15 minutes
     };
 
-    const setTimeLeftUntilNextUpdate = () => {
+    const getMapIndex = () => {
+      return pseudoRandom(getCurrentSeed(), maps.length);
+    };
+
+    const calculateTimeLeft = () => {
       const now = new Date();
-      const nextUpdate = new Date(now);
-      nextUpdate.setMinutes(
-        nextUpdate.getMinutes() + 15 - (nextUpdate.getMinutes() % 15),
-        0,
+      const nextChange = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        now.getHours(),
+        now.getMinutes() - (now.getMinutes() % 15) + 15,
         0
       );
-      return Math.ceil((nextUpdate - now) / 1000);
+      return Math.ceil((nextChange - now) / 1000);
     };
 
-    const updateMap = () => {
+    const updateMapAndTimer = () => {
       setSelectedMap(maps[getMapIndex()]);
-      setTimeLeft(setTimeLeftUntilNextUpdate());
+      setTimeLeft(calculateTimeLeft());
     };
 
-    const timerInterval = setInterval(() => {
-      setTimeLeft((prevTime) => {
-        let time = prevTime - 1;
-        if (time <= 0) {
-          updateMap();
-          time = setTimeLeftUntilNextUpdate();
-        }
-        return time;
-      });
-    }, 1000);
-
-    updateMap();
+    updateMapAndTimer(); // Initial update
+    const timerInterval = setInterval(updateMapAndTimer, 1000); // Update every second
 
     return () => clearInterval(timerInterval);
   }, []);
