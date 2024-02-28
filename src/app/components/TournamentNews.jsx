d5bf530b0aec7cb149c9b1c4c8e008932146d3b8
CodeCraftKJ <krzysztofjofficial@gmail.com>
2024-02-28 20:40:45 +0100

    Update TournamentNews.jsx

--- src/app/components/TournamentNews.jsx
+++ src/app/components/TournamentNews.jsx
@@ -402,7 +402,8 @@ const NewsSection = () => {
           <p className="text-lg mt-4 mb-6">
             The official website for the Crusader 2 Winter Championship has been
             launched. Website made by Letonetma, The Creator of Maps and Rules:
-            Hawt.
+            Hawt.If you want to stream games or help in any other ways, contact
+            us on steam
           </p>
         </div>
         <p className="py-6">&nbsp;</p>
