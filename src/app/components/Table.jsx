a0cfcd00b8a553828d76143111cd71b3bf7a1a02
CodeCraftKJ <krzysztofjofficial@gmail.com>
2025-02-27 16:42:49 +0100

    Update Table.jsx

--- src/app/components/Table.jsx
+++ src/app/components/Table.jsx
@@ -13,6 +13,7 @@ const players = [
   { name: "CasusObliquus", won: 0, lost: 0, drawn: 0 },
   { name: "ILLYRIAN", won: 2, lost: 2, drawn: 0 },
   { name: "nsvsochi", won: 0, lost: 0, drawn: 0 },
+  { name: "Gioni", won: 0, lost: 0, drawn: 0 },
 ];
 
 const specialColors = ["#8A2BE2", "#8A2BE2", "#7B68EE", "#7B68EE"]; // Niebiesko-fioletowe
